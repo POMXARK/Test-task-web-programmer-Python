67b1a0853429e2a4e1aad54ee9f78e2f39ff1d71
POMXARK <1997pom@gmail.com>
2021-09-28 23:09:49 +1000

    Django + Vue.js + axios + django rest + sort_table

--- Test_task_web_programmer_Python/Test_task_web_programmer_Python/settings.py
+++ Test_task_web_programmer_Python/Test_task_web_programmer_Python/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap5', # connecting styles
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
